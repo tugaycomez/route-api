b0aa2df3f9a9aac35c789e7c268a1c6c867fbae2
tugaycomez <tugaycomez@gmail.com>
2026-05-12 17:04:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -34,7 +34,37 @@ def route_cost(data: dict):
         "key": GOOGLE_API_KEY
     })
 
-    route = r.json()["routes"][0]["legs"][0]
+   routes = r.json()["routes"]
+
+result = []
+
+for route in routes[:3]:
+
+    leg = route["legs"][0]
+
+    km = leg["distance"]["value"] / 1000
+    time_h = leg["duration"]["value"] / 3600
+
+    speed = km / time_h
+
+    if speed <= 90:
+        fuel_rate = 4
+    elif speed <= 110:
+        fuel_rate = 5
+    else:
+        fuel_rate = 6
+
+    fuel_l = (km * fuel_rate) / 100
+    fuel_price = 63.5
+
+    result.append({
+        "km": km,
+        "time_min": time_h * 60,
+        "fuel_l": fuel_l,
+        "fuel_tl": fuel_l * fuel_price
+    })
+
+return {"routes": result}
 
     km = route["distance"]["value"] / 1000
     time_h = route["duration"]["value"] / 3600
