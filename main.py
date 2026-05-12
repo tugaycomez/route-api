e0be9614f0c5b4fd83410f6d62352dab95acbee0
tugaycomez <tugaycomez@gmail.com>
2026-05-12 15:42:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+import os
+import requests
+
+app = FastAPI()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
+
+@app.post("/route-cost")
+def route_cost(data: dict):
+
+    origin = data["origin"]
+    destination = data["destination"]
+
+    url = "https://maps.googleapis.com/maps/api/directions/json"
+
+    r = requests.get(url, params={
+        "origin": origin,
+        "destination": destination,
+        "key": GOOGLE_API_KEY
+    })
+
+    route = r.json()["routes"][0]["legs"][0]
+
+    km = route["distance"]["value"] / 1000
+    time_h = route["duration"]["value"] / 3600
+
+    speed = km / time_h
+
+    if speed <= 90:
+        fuel_rate = 4
+    elif speed <= 110:
+        fuel_rate = 5
+    else:
+        fuel_rate = 6
+
+    fuel_l = (km * fuel_rate) / 100
+    fuel_price = 63.5
+
+    return {
+        "km": km,
+        "time_min": time_h * 60,
+        "fuel_l": fuel_l,
+        "fuel_tl": fuel_l * fuel_price
+    }
